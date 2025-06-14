0440102979bc6fc42ce647bcfa6906e0ae14a68f
KeshavLab <kolekarkeshav38@gmail.com>
2025-06-14 13:39:30 +0530

    commit

--- src/operatorsAndBinaryNumberSystem.java
+++ src/operatorsAndBinaryNumberSystem.java
@@ -37,12 +37,53 @@ public class operatorsAndBinaryNumberSystem {
 
         // post decrement
 
-        int a=10;
-        int b=0;   // sabase pehalle ham  b ko value denge  10 and  usake bad a ko change karenge yani a-- (10-1=9) then a=9
+//        int a=10;
+//        int b=0;   // sabase pehalle ham  b ko value denge  10 and  usake bad a ko change karenge yani a-- (10-1=9) then a=9
+//
+//        b= a--;
+//        System.out.println(a);
+//        System.out.println(b);
 
-        b= a--;
-        System.out.println(a);
-        System.out.println(b);
+        // Relational operators
+//        two values ke bich me relation batate hai
+        //  relational oprators ka jo output hota hai vo hamehsha boolean hota hai
+//        types:
+//        1)"==" - is equal to
+//        2)"!=" - is not equal to
+//        3)">" - greater than equal to
+//        4)"<" - less than  equal to
+//        5)">=" -grater than equal to
+//        6)"<=" - less than equal to )
+
+
+         // Logical oprerator
+//        types:
+//        1)"&&" - logical AND
+//        2)"||" - logical OR
+//        3)"!" - logical NOT
+
+
+        // Bitwise OPerators
+//      types :
+//        1)  "&" - binary AND Operator
+//        2) "|" - binary OR
+//        3) "^" - Binary XOR (similar values hogi to "false" return karega & different values hogi to "true" return karega)
+//        4) "~" - binary One's complement ( bits ko oposite kar deta hai yani (1 ko 0) & (0 ko 1) ex- (1001 - 0110) )
+//                   hame bits ke andar akbhi bhi jab (+ve number ko -ve) number me ya (-ve number ko +ve) me convert karana hota hai
+//                   tab hum complement  operators ka use karate hai
+//        5) "<<" - Binary left shift
+//        6) ">>" - Binary right shift
+
+        //Assignment Operators
+//        1)"=" - assignment operator
+//        2)"+=" - add and assignment operator
+//        3)"-=" - substract and assignment operator
+//        4)"*=" - multiply and assignment operator
+//        5)"/=" - divide and assignment operator
+
+//        for ex:- a= a*b iska shortcut a*=b  
+
+
 
 
     }
