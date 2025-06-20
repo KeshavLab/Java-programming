2eb59562d4fefbe5a16525fed5dd3007ed2ec5fa
KeshavLab <kolekarkeshav38@gmail.com>
2025-06-20 14:32:15 +0530

    constructor

---
+++ src/constructor.java
@@ -0,0 +1,32 @@
+
+
+class Mobile{
+    String name;
+    float price;
+
+
+    //constructor parameterized
+    Mobile(String mobileName, int mobilePrice){
+        name=mobileName;
+        price=mobilePrice;
+    }
+
+    // to display the mobile information
+    public void mobileInfo(){
+        System.out.println("name ="+" "+name);
+        System.out.println("name ="+" "+price);
+    }
+}
+
+
+
+public class constructor {
+    public static void main(String[] args){
+
+        Mobile m1= new Mobile("oppo",20000);
+
+        // print info
+        m1.mobileInfo();
+
+    }
+}
