79ae37d783875e77a8630fba7a67180750694261
KeshavLab <kolekarkeshav38@gmail.com>
2025-06-24 15:34:11 +0530

    M

---
+++ src/arrayList.java
@@ -0,0 +1,16 @@
+//| Method                              | Description                                           |
+//| ----------------------------------- | ----------------------------------------------------- |
+//| `add(E e)`                          | Adds an element                                       |
+//| `addAll(Collection<? extends E> c)` | Adds all elements from another collection             |
+//| `remove(Object o)`                  | Removes a specific element                            |
+//| `removeAll(Collection<?> c)`        | Removes all matching elements from another collection |
+//| `clear()`                           | Removes all elements                                  |
+//| `contains(Object o)`                | Checks if element exists                              |
+//| `containsAll(Collection<?> c)`      | Checks if all elements from another collection exist  |
+//| `isEmpty()`                         | Checks if the collection is empty                     |
+//| `size()`                            | Returns number of elements                            |
+//| `iterator()`                        | Returns an iterator for looping                       |
+//| `toArray()`                         | Converts collection to array                          |
+
+public class arrayList {
+}
