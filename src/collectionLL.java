8d832888df990a609026d1157643a25becc77f50
KeshavLab <kolekarkeshav38@gmail.com>
2025-06-26 15:08:48 +0530

    ll

--- src/collectionLL.java
+++ src/collectionLL.java
@@ -7,8 +7,24 @@ public class collectionLL {
         LinkedList<String> list= new LinkedList<>();
         list.addFirst("a");
         list.addFirst("is");
-        list.addLast("This");
         System.out.println(list);
+        list.addFirst("This");
+        list.addLast("list");
+        System.out.println(list);
+        System.out.println(list.size());
+
+        // traverse the list
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+" "+"->");
+        }
+        System.out.println("null");
+
+        list.removeFirst();
+        System.out.println(list);
+
+        list.remove(2);
+        System.out.println(list);
+
 
     }
 }
