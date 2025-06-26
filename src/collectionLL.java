996d1725847c527ece243821a1abe9c58e5b243f
KeshavLab <kolekarkeshav38@gmail.com>
2025-06-26 14:58:27 +0530

    ll

---
+++ src/collectionLL.java
@@ -0,0 +1,14 @@
+// Linked List using Collection framework
+
+import java.util.*;
+public class collectionLL {
+    public static void main(String[] args){
+        //Initialize  the linkedlist using ollection framework
+        LinkedList<String> list= new LinkedList<>();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.addLast("This");
+        System.out.println(list);
+
+    }
+}
